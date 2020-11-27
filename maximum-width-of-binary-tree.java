325b61745b23ad8c1df4c53b6e2d2c1da8051659
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-11-26 22:52:18 -0600

    working commit - Created using LeetHub

---
+++ maximum-width-of-binary-tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        LinkedList<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        root.val = 0;
+        int max = 1;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            max = Math.max(max, q.peekLast().val - q.peekFirst().val + 1);
+            for (int i = 0; i < size; i++) {
+                root = q.poll();
+                if (root.left != null) {
+                    root.left.val = root.val * 2;
+                    q.add(root.left);
+                }
+                if (root.right != null) {
+                    root.right.val = root.val * 2 + 1;
+                    q.add(root.right);
+                }
+            }
