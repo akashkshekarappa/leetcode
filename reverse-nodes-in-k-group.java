5e10223443f62918cf667afd0a52a021aafdff79
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-11-26 18:29:18 -0600

    working commit - Created using LeetHub

---
+++ reverse-nodes-in-k-group.java
@@ -0,0 +1,45 @@
+     * 
+     * 0->3->2->1->4->5->6
+     *          |  |
+     *      begin end
+     * @return the reversed list's 'begin' node, which is the precedence of node end
+     */
+class Solution {
+   public ListNode reverseKGroup(ListNode head, int k) {
+       ListNode begin;
+       if (head==null || head.next ==null || k==1)
+           return head;
+       ListNode dummyhead = new ListNode(-1);
+       dummyhead.next = head;
+       begin = dummyhead;
+       int i=0;
+        
+       while (head != null){
+           i++;
+           if (i%k == 0){
+               begin = reverse(begin, head.next);
+               head = begin.next;
+           } 
+           else {
+               head = head.next;
+           }
+       }
+       return dummyhead.next;
+   }
+    public ListNode reverse(ListNode begin, ListNode end){
+        ListNode curr = begin.next;
+        ListNode next, first;
+        ListNode prev = begin;
+        first = curr;
+        
+        while (curr!=end){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        begin.next = prev;
+        first.next = curr;
+        return first;
+    }
+}
