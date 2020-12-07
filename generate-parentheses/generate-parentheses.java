839308c09b2c94283e24633d1085c31d1cfbd14f
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-12-06 22:08:37 -0600

    working commit - Created using LeetHub

---
+++ generate-parentheses/generate-parentheses.java
@@ -0,0 +1,23 @@
+//Backtracking
+//TC: O(4^n / sqrt(n)) => O(n)
+//SC: O(4^n / sqrt(n)) => O(n)
+class Solution {
+    
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+    private void backtrack( List<String> result, String curr, int open, int close, int n){
+        if(curr.length() == n * 2){
+            result.add(curr);
+            return;
+        }
+        if(open < n){
+            backtrack(result, curr+"(" , open +1, close, n);
+        }
+        if(close < open){
+            backtrack(result, curr+")", open, close +1, n);
+        }
+    }
+}
