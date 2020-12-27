a777857043597d7a77985cbde257c927b6391889
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-12-27 15:56:49 -0600

    Time: 1 ms (98.76%), Space: 37.5 MB (22.19%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.java
@@ -0,0 +1,20 @@
+//TC: O(n)
+//SC: O(n)
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);   //curr character
+            //push closing bracket corresponding to open ones 
+            if(c == '(')
+                stack.push(')');
+            else if(c == '{')
+                stack.push('}');
+            else if(c == '[')
+                stack.push(']');
+            else if(stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();  // if stack is empty then it is valid
+    }
+}
