c94d926749389c4f5375f79d06703b73d23e0a5f
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-12-27 11:14:09 -0600

    Time: 10 ms (53.74%), Space: 42.5 MB (29.52%) - LeetHub

--- brick-wall/brick-wall.java
+++ brick-wall/brick-wall.java
@@ -1,27 +1,24 @@
-//TC: O(n)
-//SC: O()
+//TC: O(n),We traverse over the complete bricks only once. n is the total number of bricks in a wall.
+//SC: O(m),mapmap will contain atmost m entries, where m refers to the width of the wall.
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
-        int max = 0;
         Map<Integer, Integer> freqmap = new HashMap<>();
         int prefixSum = 0;
         
         for(List<Integer> row: wall){
             prefixSum = 0;
-            for(int n : row){
-                prefixSum+= n;
+            for(int i=0; i<row.size()-1; i++){
+                prefixSum+= row.get(i);
                 freqmap.put(prefixSum, freqmap.getOrDefault(prefixSum,0)+1);
             }
         }
         
+        int result = wall.size();
         for(int key: freqmap.keySet()){
-            
-            if(key == prefixSum)
-                continue;
-            max = Math.max(max, freqmap.get(key));
+            result = Math.min(result, wall.size() - freqmap.get(key));
         }
         
-        return wall.size() - max;
+        return result;
     }
 }
 ​
@@ -30,6 +27,4 @@ Basic idea is in each row, we need to know if a line falls at the edge of a bric
 ​
 So all we need is a brick whose length ends at the same point across maximum rows.
 So we just record prefix sum frequency and choose the maximum. But for the answer, we need how many rows are touched. So total# of rows - max rows not touched.
-Also note that we shouldn't consider the line passing at the edge of all rows, that is why
-if(key == pre) continue;
 */
