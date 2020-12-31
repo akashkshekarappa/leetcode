a268ee53b337255cd46ff71c887b61df1a28dd0c
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-12-31 12:02:37 -0600

    Time: 0 ms (100.00%), Space: 39 MB (28.30%) - LeetHub

--- reverse-linked-list/reverse-linked-list.java
+++ reverse-linked-list/reverse-linked-list.java
@@ -10,18 +10,31 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null)
-            return head;
-        
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode fast = head.next;
-        
-        while(curr.next != null){
-            curr.next = prev;
-            prev = curr;
-            curr = fast;
-            fast = fast.next;
-        }
-        curr.next = prev;
-        
+        if(head == null || head.next == null)   // base condition, if 0 node or 1 node 
+            return head;
+​
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode fast = head.next;  // fast pointer, so we dont loose the connection to next node where curr is supposed to go
+​
+        while(curr.next != null){   // till the next node is null
+            curr.next = prev;    // change the direction of currentNode.next to point to prev
+            prev = curr;    // point prev to current    
+            curr = fast;    // point curr to fast 
+            fast = fast.next;   // shift fast to next position
+        }
+        curr.next = prev;   // as the loop misses the last node, so change the currentNode.next to point to prev
+​
+        return curr; 
+    }   
+}
+​
+//Recursion
+//O(n), O(n)
+//public ListNode reverseList(ListNode head) {
+//    if (head == null || head.next == null) return head;
+//    ListNode p = reverseList(head.next);
+//    head.next.next = head;
+//    head.next = null;
+//    return p;
+//}
