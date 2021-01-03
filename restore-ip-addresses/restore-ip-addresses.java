b05c8f33b3cb6fe67e62ba1d16d72abda2c77b1b
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-01-02 21:57:16 -0600

    Time: 4 ms (60.54%), Space: 40 MB (7.45%) - LeetHub

--- restore-ip-addresses/restore-ip-addresses.java
+++ restore-ip-addresses/restore-ip-addresses.java
@@ -1,6 +1,9 @@
 //Backtracking
-//TC: 
-//SC:
+//TC: O(1)
+//SC: O(1)
+/*If one already put a dot that leaves only 3 possibilities for the next dot to be placed : after one digit, after two digits, or after three digits. The first dot has only 3 available slots as well.
+​
+That propagates constraints and helps to reduce a number of combinations to consider. Instead of 990990 combinations it's enough to check just  3×3×3=27. */
 class Solution {
     List<String> result;
     public List<String> restoreIpAddresses(String s) {
@@ -24,7 +27,7 @@ class Solution {
                 break;
             
             int num = Integer.valueOf(s.substring(index, index+i));
-            if(i==1 || i==2 && num >= 10 && num <= 99 || i==3 && num >= 100 && num <= 255){
+            if(i==1 || i==2 && num >= 10 && num <= 99 || i==3 && num >= 100 && num <= 255){//IP definition
                 //action
                 sb.append(num);
                 
@@ -36,3 +39,19 @@ class Solution {
                 
                 //backtrack
                 if(level < 3)
+                    sb.deleteCharAt(sb.length()-1);
+                sb.delete(sb.length()-i, sb.length());
+            }
+        }
+    }
+}
+​
+​
+​
+/*
+Definition of valid IP address:
+1. The length of the ip without '.' should be equal to the length of s;
+2. The digit order of ip should be same as the digit order of s;
+3. Each part separated by the '.' should not start with '0' except only '0';
+4. Each part separared by the '.' should not larger than 255;
+*/
