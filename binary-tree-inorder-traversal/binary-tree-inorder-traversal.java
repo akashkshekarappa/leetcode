442b4b027e5440278cc31be94cb5f75a34591787
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-01-11 22:31:17 -0600

    Time: 0 ms (100.00%), Space: 37.4 MB (44.16%) - LeetHub

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -33,3 +33,24 @@ class Solution {
         return result;
     }
 }
+​
+//Recursion
+/*class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(root, result);
+        return result;
+    }
+    private void helper(TreeNode root, List<Integer> result){
+        if(root != null){
+            if(root.left != null)
+                helper(root.left, result);
+            
+            result.add(root.val);
+            
+            if(root.right != null)
+                helper(root.right, result);
+        }
+    }
+}*/
+​
