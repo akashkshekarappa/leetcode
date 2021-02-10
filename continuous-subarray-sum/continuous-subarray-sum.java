a45f5c5b383aa37b509813d7b9d555c83fd64977
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-02-09 20:01:06 -0600

    Time: 1614 ms (5.05%), Space: 39.8 MB (51.63%) - LeetHub

---
+++ continuous-subarray-sum/continuous-subarray-sum.java
@@ -0,0 +1,17 @@
+//Brute Force
+//TC: O(N ^3)
+//SC: O(1)
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        for(int start=0; start<nums.length-1; start++){
+            for(int end=start+1; end<nums.length; end++){
+                int sum = 0;
+                for(int i=start; i<=end; i++)
+                    sum+= nums[i];
+                if(sum == k || (k != 0 && sum % k == 0))
+                    return true;
+                }
+            }
+        return false;
+    }
+}
