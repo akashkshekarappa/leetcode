7ba8707f7f811bc14da307175c940dec4c42ecae
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-02-11 09:53:30 -0600

    Time: 3 ms (99.46%), Space: 55.1 MB (49.09%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,23 @@
+//One pass
+//TC: O(n)
+//SC: O(1)
+class Solution {
+    public int maxProfit(int[] prices) {
+        //edge case
+        if(prices == null || prices.length == 0)
+            return 0;
+        
+        int buy1 = Integer.MAX_VALUE; int sell1 = 0;
+        int buy2 = Integer.MAX_VALUE; int sell2 = 0;
+        
+        for(int i=0; i<prices.length; i++){
+            buy1 = Math.min(buy1, prices[i]);
+            sell1 = Math.max(sell1, prices[i] - buy1);
+            
+            //include the profit from first transaction in second ones
+            buy2 = Math.min(buy2, prices[i] - sell1);
+            sell2 = Math.max(sell2, prices[i] - buy2);
+        }
+        return sell2;
+    }
+}
