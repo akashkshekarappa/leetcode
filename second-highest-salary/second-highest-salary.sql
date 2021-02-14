a84e01c879d1b955a5164c3321a3c632af1fc7be
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-02-14 17:16:23 -0600

    Time: 269 ms (13.64%), Space: 0B (100.00%) - LeetHub

--- second-highest-salary/second-highest-salary.sql
+++ second-highest-salary/second-highest-salary.sql
@@ -2,3 +2,9 @@
 select max(Salary) as SecondHighestSalary
 from Employee
 where Salary < (select max(Salary) from Employee) 
+
+#select
+#(select distinct Salary
+#from Employee
+#order by Salary Desc
+#Limit 1 offset 1) as SecondHighestSalary
