020b97cf900deb5018464b5ccf34f32e972ce4d1
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-02-14 17:10:58 -0600

    Time: 5 ms (99.16%), Space: 40.4 MB (24.66%) - LeetHub

---
+++ queue-reconstruction-by-height/queue-reconstruction-by-height.java
@@ -0,0 +1,25 @@
+//Greedy
+//TC: O(N^2)
+//SC: O(N)
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        //sort the array first based on height
+        // Ip: [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+        //sort : [[7,0],[7,1],[6,1],[5,0],[5,2],[4,4]]
+        Arrays.sort(people, (a,b) ->{
+            if(a[0] == b[0])
+                return a[1] - b[1];
+            
+            return b[0] - a[0];
+        });
+        
+        // now place the above sorted array according to index
+        // Op: [[[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]]
+        List<int[]> result = new ArrayList<>();
+        for(int i=0; i<people.length; i++){
+            int[] person = people[i];
+            result.add(person[1], person);
+        }
+        return result.toArray(new int[0][]);
+    }
+}
