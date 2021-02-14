3cc5f6d7c3ece2089ae50527e8f89d7445b13234
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-02-14 14:38:33 -0600

    Time: 20 ms (45.54%), Space: 40 MB (51.34%) - LeetHub

---
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,35 @@
+//TC: O(n)
+//S: O(n)
+
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Set<Integer> indexToRemove = new HashSet<>();
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                st.push(i);
+            }
+            
+            if(s.charAt(i) == ')'){
+                if(st.isEmpty()){
+                    indexToRemove.add(i);
+                }
+                else
+                    st.pop();
+            }
+        }
+        
+         // Put any indexes remaining on stack into the set.
+        while(!st.isEmpty()){
+            indexToRemove.add(st.pop());
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            if(!indexToRemove.contains(i))
+                sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
