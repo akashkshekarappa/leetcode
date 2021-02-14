50e495948f4c4203b80ce71564e3e6ca36bb9501
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-02-14 09:07:03 -0600

    Time: 0 ms (100.00%), Space: 36.7 MB (87.76%) - LeetHub

---
+++ pascals-triangle/pascals-triangle.java
@@ -0,0 +1,18 @@
+//TC: O(numRows^2)
+//SC: O(numRows^2)
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0; i<numRows; i++){
+            List<Integer> rows = new ArrayList<>();
+            for(int j=0; j<=i; j++){
+                if(j == 0 || j == i)
+                    rows.add(1);
+                else
+                    rows.add(result.get(i-1).get(j-1) + result.get(i-1).get(j));
+            }
+            result.add(rows);
+        }
+        return result;
+    }
+}
