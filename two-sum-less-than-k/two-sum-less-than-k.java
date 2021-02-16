3aea970f2e2a74101e236a3fd8da3b18d6888c06
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-02-15 22:44:18 -0600

    Time: 1 ms (100.00%), Space: 38.4 MB (58.74%) - LeetHub

---
+++ two-sum-less-than-k/two-sum-less-than-k.java
@@ -0,0 +1,24 @@
+//Two Pointers
+//TC: O(n logn)
+//SC: O(logn)
+class Solution {
+    public int twoSumLessThanK(int[] A, int K) {
+        //edge case
+        if(A == null || A.length == 0)
+            return 0;
+        Arrays.sort(A);
+        int low = 0;
+        int high = A.length -1;
+        int s = -1;
+        while(low < high){
+            int sum = A[low] + A[high];
+            if(sum < K){
+                s = Math.max(s, A[low] + A[high]);
+                low ++;
+            }
+            else
+                high --;
+        }
+        return s;
+    }
+}
