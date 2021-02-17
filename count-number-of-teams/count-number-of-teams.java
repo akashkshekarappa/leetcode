fa64b6729cdbe835647fbb3234267f03b865b62d
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-02-16 18:23:34 -0600

    Time: 23 ms (48.42%), Space: 38.5 MB (47.68%) - LeetHub

---
+++ count-number-of-teams/count-number-of-teams.java
@@ -0,0 +1,30 @@
+//Optimized
+//TC: O(n^2)
+//SC: O(1)
+class Solution {
+    public int numTeams(int[] rating) {
+        int n = rating.length;
+        int count = 0;
+        
+        for(int j=0; j<n; j++){
+            int leftSmaller = 0, rightLarger = 0;
+            int leftLarger = 0, rightSmaller = 0;
+            for(int i=0; i<j; i++){
+                if(rating[i] < rating[j])
+                    leftSmaller++;
+                else if(rating[i] > rating[j])
+                    leftLarger++;
+            }
+            
+            for(int k=j+1; k<n; k++){
+                if(rating[j] < rating[k])
+                    rightLarger++;
+                else if(rating[j] > rating[k])
+                    rightSmaller++;
+            }
+            
+            count+= leftSmaller * rightLarger + leftLarger * rightSmaller;
+        }
+        return count;
+    }
+}
