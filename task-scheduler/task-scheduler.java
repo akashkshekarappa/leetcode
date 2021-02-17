bd3842fcfaf6934b1a837fd89a31259135878121
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-02-17 08:57:37 -0600

    Time: 11 ms (39.09%), Space: 39.5 MB (94.46%) - LeetHub

---
+++ task-scheduler/task-scheduler.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        //edge case
+        if(tasks == null || tasks.length == 0)
+            return 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int maxCount = 0;
+        int maxFreq = 0;
+        // map with count of each task and keeping track of max frequency of task
+        for(char task: tasks){
+            int count = map.getOrDefault(task, 0) + 1;
+            maxFreq = Math.max(maxFreq, count);
+            map.put(task, count);
+        }
+        //iterate over map and if task is equal to maxFreq, then increment maxCount
+        for(char c: map.keySet()){
+            if(map.get(c) == maxFreq)
+                maxCount++;
+        }
+        
+//         int partitions = maxFreq - 1;
+//         int empty = partitions * (n - maxCount + 1);
+//         int pending = tasks.length - (maxFreq * maxCount);
+//         int idle = Math.max(0, empty - pending);
+        
+//         return idle + tasks.length;
+        
+        return Math.max(tasks.length, (maxFreq - 1) * (n+1) + maxCount);
+    }
+}
