4da238922e0bc6570d2057f4c561156cf9dda9d1
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-02-17 09:31:50 -0600

    Time: 0 ms (100.00%), Space: 39.4 MB (24.20%) - LeetHub

---
+++ rotate-image/rotate-image.java
@@ -0,0 +1,24 @@
+//Approac: Transpose and then reverse
+//TC: O(n^2)
+//SC: O(1)
+class Solution {
+    public void rotate(int[][] matrix) {
+        //Step: Transpose , turns rows to column
+        for(int i=0; i<matrix.length; i++){
+            for(int j=i; j<matrix.length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        //Step: reverse each row
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix.length/2; j++){  // by 2 because we only reach till mid and swap
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 -j];
+                matrix[i][matrix.length - 1 -j] = temp;
+            }
+        }
+    }
+}
