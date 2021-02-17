dd6c6002eb5a5f5ab89751d9edbc18ff469f067f
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-02-16 21:57:52 -0600

    Time: 272 ms (6.89%), Space: 39.9 MB (68.66%) - LeetHub

--- find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -1,17 +1,20 @@
-//TC:O(k log k)
-//SC: O(log k)
+// Need to optimize
 class Solution {
-    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<List<Integer>> pq = new PriorityQueue<>((a,b) -> (b.get(0) + b.get(1) - a.get(0) - a.get(1)));// min queue, sorted by pair sum
-        
-        for(int i=0; i<nums1.length; i++){
-            for(int j=0; j<nums2.length; j++){
-                pq.add(Arrays.asList(nums1[i], nums2[j]));
-                
-                if(pq.size() > k)
-                    pq.poll();
-            }
-        }
-        return new ArrayList<>(pq);
-    }
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<>((a,b) -> (b.get(0) + b.get(1) - a.get(0) - a.get(1)));// max queue, sorted by pair sum
+        
+        for(int i=0; i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
+                pq.add(Arrays.asList(nums1[i], nums2[j]));
+                             
+                if(pq.size() > k)
+                    pq.poll();  
+                /* // elements are in pq at final
+                |1, 6|
+                |1, 4|
+                |1, 2| */
+            }
+        }
+        return new ArrayList<>(pq);
+    }
 }
