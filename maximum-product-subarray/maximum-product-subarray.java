35ca667a057820dced788af628fc818f0abd28dc
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-04-27 22:39:26 -0500

    Time: 1 ms (91.53%), Space: 39 MB (22.03%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.java
+++ maximum-product-subarray/maximum-product-subarray.java
@@ -1,3 +1,29 @@
+//DP
+//TC: O(n)
+//SC: O(1)
+class Solution {
+    public int maxProduct(int[] nums) {
+        //edge case
+        if(nums == null || nums.length == 0)
+            return 0;
+        int maxSoFar = nums[0];
+        int minSoFar = nums[0];
+        int result = maxSoFar;
+        for(int i=1; i<nums.length; i++){
+            int curr = nums[i];
+            int tempMax = Math.max(curr, Math.max(maxSoFar * curr, minSoFar * curr));
+            minSoFar = Math.min(curr, Math.min(maxSoFar * curr, minSoFar * curr));
+            
+            maxSoFar = tempMax;
+            
+            result = Math.max(result, maxSoFar);
+        }
+        return result;
+    }
+}
+
+
+/*
 //Brute Force
 //TC: O(n^2)
 //SC: O(1)
@@ -17,4 +43,4 @@ class Solution {
         }
         return maxProduct;
     }
-}
+}*/
