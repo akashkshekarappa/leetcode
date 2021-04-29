ccb96921f774f441af9e3d4a9ab498f78c333627
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-04-28 21:30:15 -0500

    Time: 1 ms (94.60%), Space: 45.7 MB (66.16%) - LeetHub

---
+++ reverse-string/reverse-string.java
@@ -0,0 +1,17 @@
+//Two pointers
+//TC: O(n)
+//SC: O(1)
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length -1;
+        while(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
