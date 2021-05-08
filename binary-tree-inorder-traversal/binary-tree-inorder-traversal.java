58e0d38e45f3289b29e7e2713e97c9a1cd30abb7
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-05-08 17:56:19 -0500

    Time: 0 ms (100.00%), Space: 37.2 MB (64.61%) - LeetHub

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,56 +1,55 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 //Iterative Method, stack
 //TC:O(n)
-//SC:O(n)
+//SC:O(n), and in the average case it's O(logn) where n is number of nodes.
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        //TreeNode curr = root;
-        while(root != null || !stack.isEmpty()){
-            while(root != null){
-                stack.push(root);
-                root = root.left;   
-            }
-            root = stack.pop();
-            result.add(root.val);
-            root = root.right;
-        }
-        return result;
-    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        //TreeNode curr = root;
+        while(root != null || !stack.isEmpty()){
+            while(root != null){
+                stack.push(root);
+                root = root.left;   
+            }
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+        }
+        return result;
+    }
 }
-​
+
 //Recursion
 /*class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        helper(root, result);
-        return result;
-    }
-    private void helper(TreeNode root, List<Integer> result){
-        if(root != null){
-            if(root.left != null)
-                helper(root.left, result);
-            
-            result.add(root.val);
-            
-            if(root.right != null)
-                helper(root.right, result);
-        }
-    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(root, result);
+        return result;
+    }
+    private void helper(TreeNode root, List<Integer> result){
+        if(root != null){
+            if(root.left != null)
+                helper(root.left, result);
+            
+            result.add(root.val);
+            
+            if(root.right != null)
+                helper(root.right, result);
+        }
+    }
 }*/
-​
