62e24d145109b5cd21fe757937cdeb61cdd81494
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-08-18 21:49:54 -0500

    Time: 5 ms (99.37%), Space: 39.4 MB (57.88%) - LeetHub

---
+++ longest-word-in-dictionary/longest-word-in-dictionary.java
@@ -0,0 +1,26 @@
+//TC: O(w^2), W is length of words[i]
+class Solution {
+    public String longestWord(String[] words) {
+        String result = "";
+        Set<String> set = new HashSet<>();
+        for(String word: words){
+            set.add(word);
+        }
+        
+        for(String word: words){
+            if(word.length() > result.length() || word.length() == result.length() && word.compareTo(result) < 0){
+                boolean good = true;
+                
+                for(int k=1; k<word.length(); k++){
+                    if(!set.contains(word.substring(0, k))){
+                        good = false;
+                        break;
+                    }
+                }
+                if(good)
+                    result = word;
+            }
+        }
+        return result;
+    }
+}
