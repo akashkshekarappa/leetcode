0a0d5ee9638170d87401f6f7d665f8366750699a
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-09-01 20:46:48 -0500

    Time: 0 ms (100.00%), Space: 38.4 MB (5.96%) - LeetHub

--- angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.java
+++ angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.java
@@ -1,3 +1,5 @@
+//Time complexity: O(1)
+//Space Complexity: O(1)
 class Solution {
     public double angleClock(int hour, int minutes) {
         float h = (hour % 12 + (float)minutes/60) * 30;    //position of hour, 360 deg / 12 hours = 30 degrees per hours
