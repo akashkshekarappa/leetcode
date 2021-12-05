3471414d561c8f689b7bff2cb1be5705dd966313
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2021-12-05 10:05:50 -0600

    Time: 0 ms (100.00%), Space: 38.1 MB (99.86%) - LeetHub

--- house-robber-iii/house-robber-iii.java
+++ house-robber-iii/house-robber-iii.java
@@ -13,25 +13,21 @@
  *     }
  * }
  */
-//Recursion
-//Time Complexity: O(n)
-//Space Complexity: O(n)
 class Solution {
     public int rob(TreeNode root) {
         int[] result = helper(root);
         return Math.max(result[0], result[1]);
     }
-    public int[] helper(TreeNode root){
-        if(root == null)    // return [rob this node, not rob this node]
+    
+    private int[] helper(TreeNode root){
+        if(root == null)
             return new int[] {0, 0};
         
         int[] left = helper(root.left);
         int[] right = helper(root.right);
         
-        // if we rob this node, we cannot rob its children
         int rob = root.val + left[1] + right[1];
         
-        // else, we free to choose rob its children or not
         int notRob = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
         
         return new int[] {rob, notRob};
