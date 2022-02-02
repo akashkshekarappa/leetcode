f40282a5a01330afab02860ce2a47d452f0b2a5b
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2022-02-01 19:46:28 -0600

    Time: 26 ms (41.93%), Space: 48.2 MB (16.07%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,35 +1,35 @@
+//TC: O(m + n)
+//S: O(n)
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result = new ArrayList<>();
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i=0; i<p.length(); i++){
-            map.put(p.charAt(i), map.getOrDefault(p.charAt(i), 0) + 1);
-        }
-        
-        int slow = 0;
-        int fast = 0;
+        HashMap<Character, Integer> map = new HashMap<>();  //freq of each character of string p
         int match = 0;
+        for(int i=0; i<p.length(); i++){
+            map.put(p.charAt(i), map.getOrDefault(p.charAt(i), 0) +1);    //adding frequeny for each character
+        }
+        int slow = 0, fast = 0;
         
         while(fast < s.length()){
             char in = s.charAt(fast);
             if(map.containsKey(in)){
-                int count = map.get(in);
-                count--;
-                map.put(in, count);
-                
-                if(count == 0)
-                    match++;
+                int count = map.get(in);    
+				count --;    //reduce the count, if character found
+				map.put(in, count);     //updating the count
+
+				if(count == 0)  //when count becomes zero, thers match
+					match++;
             }
             
             if(fast >= p.length()){
                 char out = s.charAt(slow);
-                if(map.containsKey(out)){
-                    int count = map.get(out);
-                    count++;
-                    map.put(out, count);
-                    
-                    if(count == 1)
-                        match--;
+				if(map.containsKey(out)){
+					int count = map.get(out);
+					count++;    // increasing count for outgoing character
+					map.put(out, count);
+
+					if(count == 1)
+						match--;    //if counts is 1, then decrease match
                 }
                 slow++;
             }
