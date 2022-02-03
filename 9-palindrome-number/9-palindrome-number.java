485b52bbd285bee582e4d0a810546c1beab32312
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2022-02-02 23:49:41 -0600

    Time: 14 ms (47.71%), Space: 44.6 MB (9.84%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,17 @@
+//TC: O(log n)
+//SC: O(1)
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0)   //edge case
+            return false;
+        
+        int y = x;
+        int result = 0;
+        
+        while(y != 0){
+            result = result * 10 + y % 10;
+            y = y/10;
+        }
+        return x == result;
+    }
+}
