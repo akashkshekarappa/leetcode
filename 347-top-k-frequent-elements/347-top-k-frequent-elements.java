487765a6412b1f1177cb7d116fbfdd7b2f8ecdfe
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2022-05-26 22:42:22 -0500

    Time: 19 ms (40.83%), Space: 49.6 MB (71.24%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -6,23 +6,23 @@ class Solution {
         if(nums == null || nums.length == 0)
             return new int[0];
         
+        //map with key as nums and value as theircount
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++)
-            map.put(nums[i], map.getOrDefault(nums[i], 0) +1);
-        
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+            
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
-        
-        for(int i : map.keySet()){
+        //PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));least frequent elements
+        for(int i: map.keySet()){
             pq.add(i);
             
             if(pq.size() > k)
                 pq.poll();
         }
-        
         int[] result = new int[k];
         for(int i=k-1; i>=0; i--)
             result[i] = pq.poll();
-        
         return result;
     }
 }
