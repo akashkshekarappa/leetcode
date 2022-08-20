894e69e5c06b3721b21fa96c8845895f9d63e79b
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2022-08-20 12:27:11 -0500

    Time: 3 ms (99.16%), Space: 42.1 MB (98.81%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
@@ -1,23 +1,54 @@
-//Using dp
-//TC: O(n^2)
+///Optimized, using BFS..... Heap data structur
+
+//Using BFS
+//TC: O(n logn)
 //SC: O(n)
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
         int n = stations.length;
-        long[] dp = new long[n+1];
-        dp[0] = startFuel;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
         
-        for(int i=0; i<n; i++){
-            for(int refill = i; refill >= 0 && dp[refill] >= stations[i][0]; refill--){
-                dp[refill+1] = Math.max(dp[refill+1], dp[refill] + stations[i][1]);
+        int refill = 0;
+        int i = 0;
+        int distance = startFuel;
+        
+        while(distance < target){
+            while(i < n && distance >= stations[i][0]){
+                pq.add(stations[i]);
+                i++;
             }
-        }
-        
-        for(int i=0; i <= n; i++){
-            if(dp[i] >= target)
-                return i;
+            
+            if(pq.isEmpty())
+                return -1;
+            
+            distance += pq.poll()[1];
+            refill++;
         }
             
-        return -1;
+        return refill;
     }
 }
+
+//Using dp
+//TC: O(n^2)
+//SC: O(n)
+// class Solution {
+//     public int minRefuelStops(int target, int startFuel, int[][] stations) {
+//         int n = stations.length;
+//         long[] dp = new long[n+1];
+//         dp[0] = startFuel;
+        
+//         for(int i=0; i<n; i++){
+//             for(int refill = i; refill >= 0 && dp[refill] >= stations[i][0]; refill--){
+//                 dp[refill+1] = Math.max(dp[refill+1], dp[refill] + stations[i][1]);
+//             }
+//         }
+        
+//         for(int i=0; i <= n; i++){
+//             if(dp[i] >= target)
+//                 return i;
+//         }
+            
+//         return -1;
+//     }
+// }
